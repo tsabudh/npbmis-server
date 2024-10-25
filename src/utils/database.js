1611a1f5c92b595b2b64e47d5246a5d9ef1a6aa1
tsabudh <tsabudh@gmail.com>
2024-10-25 19:49:17 +0545

    fix: sequelize ssl removed

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,12 +2,28 @@
 
 import { Sequelize } from "sequelize";
 import pg from "pg";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize(process.env.DB_URL, "postgres", "admin", {
-  dialectModule: pg,
-  dialect: "postgres",
-  logging: false,
-  protocol: "postgres",
-});
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // Database username
+  process.env.DB_PASSWORD, // Database password
+  {
+    host: process.env.DB_HOST, // Database host
+    port: process.env.DB_PORT, // Database port
+    dialectModule: pg, // Dialect module
+    dialect: "postgres", // Use PostgreSQL dialect
+    logging: false,
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // Require SSL for secure connection
+        rejectUnauthorized: false, // Disable strict certificate verification (if needed)
+      },
+    },
+  }
+);
 
 export default sequelize;

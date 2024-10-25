eefba2a54afabedb34bcceadad891a7d73ab9a51
tsabudh <tsabudh@gmail.com>
2024-10-25 19:18:37 +0545

    chore: configured pg as dialectModule in sequelize

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,6 +3,7 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(process.env.DB_URL, "postgres", "admin", {
+  dialectModule: require("pg"),
   dialect: "postgres",
   logging: false,
   protocol: "postgres",
